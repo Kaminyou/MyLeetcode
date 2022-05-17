54c02e791a31e26a30757dc02f2d76fd06a1a1c6
Kaminyou <ikaminyou@gmail.com>
2022-05-18 00:57:35 +0800

    124

--- README.md
+++ README.md
@@ -127,6 +127,7 @@
 
 ### Tree
 - **M** [117. Populating Next Right Pointers in Each Node II](./solutions/117.cpp)
+- **H** [124. Binary Tree Maximum Path Sum](./solutions/124.cpp)
 - **M** [199. Binary Tree Right Side View](./solutions/199.cpp)
 - **H** [428. Serialize and Deserialize N-ary Tree](./solutions/428.cpp)
 - **M** [951. Flip Equivalent Binary Trees](./solutions/951.cpp)
---
+++ solutions/124.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int res = INT_MIN;
+    int dfs(TreeNode* node) {
+        if (node == nullptr) return 0;
+        
+        // if the child is negative, ignore
+        int left = max(0, dfs(node->left));
+        int right = max(0, dfs(node->right));
+        int curr = left + right + node->val;
+        res = max(res, curr);
+        
+        return max({left, right}) + node->val;
+    }
+    int maxPathSum(TreeNode* root) {
+        dfs(root);
+        return res;
+    }
+};
