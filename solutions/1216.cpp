34fa10b2caf5f361433b7cf718835f31f082c655
Kaminyou <ikaminyou@gmail.com>
2022-05-30 00:39:17 +0800

    1216

---
+++ solutions/1216.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isValidPalindrome(string s, int k) {
+        int n = s.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for (int i = 0; i < n; ++i) dp[i][i] = 1;
+        for (int i = n - 1 ; i >= 0; --i) {
+            for (int j = i + 1; j < n; ++j) {
+                if (s[i] == s[j]) dp[i][j] = dp[i + 1][j - 1] + 2;
+                else dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+            }
+        }
+        return s.size() - dp[0][n - 1] <= k;
+    }
+};
