90143816cec9ed060ffce8950aa1be257be71be2
Kaminyou <ikaminyou@gmail.com>
2022-06-02 00:19:03 +0800

    1231

--- solutions/1231.cpp
+++ solutions/1231.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
-    int seperate(const vector<int>& sweetness, int min) {
+    int separate(const vector<int>& sweetness, int min) {
+        // separate chunks by min value
+        // if min too large => chunks decrease
+        // if min too small => chunks increase
+        // want to find min that # of chunks >= k + 1
         int count = 0;
         int sum = 0;
         for (auto sweet : sweetness) {
@@ -17,11 +21,16 @@ public:
         for (auto sweet : sweetness) sum += sweet;
         int left = 0;
         int right = sum / (k + 1) + 1;
+        // +1 : [1,2,2,1,2,2,1,2,2] with k = 2
+        // if no +1 => right = 5, chunks max = 3 would not < (k+1) = 3
+        // should make right 6 so that chunks can be less than 3
         while (left < right) {
             int mid = left + (right - left) / 2;
-            if (seperate(sweetness, mid) < k + 1) right = mid;
+            if (separate(sweetness, mid) < k + 1) right = mid;
             else left = mid + 1;
         }
+        // now left is the largest value make # of chunck < k + 1
+        // so left - 1 => chunks >= k + 1
         return left - 1;
     }
 };
