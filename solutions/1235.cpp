0df1f382192256c05fe5f80f59c5c6fdbecc4bc2
Kaminyou <ikaminyou@gmail.com>
2022-06-14 10:52:17 +0800

    update 1235

--- solutions/1235.cpp
+++ solutions/1235.cpp
@@ -1,33 +1,35 @@
 class Solution {
-private:
-    vector<vector<int>> jobs;
-    vector<int> dp;
 public:
+    vector<int> dp;
+    vector<vector<int>> jobs;
+    int N;
     static bool compare(const vector<int>& v1, const vector<int>& v2) {
         return v1[0] < v2[0];
     }
-    int recursive(vector<int>& start, int curr, int n) {
-        if (curr == n) return 0;
+    int recursive(vector<int>& start, int curr) {
+        if (curr == N) return 0;
         if (dp[curr] != -1) return dp[curr];
+        // find the next => next start >= curr end
         auto it = lower_bound(start.begin() + curr, start.end(), jobs[curr][1]);
-        int p = it - start.begin();
-        dp[curr] = max(recursive(start, curr + 1, n), recursive(start, p, n) + jobs[curr][2]);
+        const int p = it - start.begin();
+        // either skip this job, or do this job
+        dp[curr] = max(recursive(start, curr + 1), recursive(start, p) + jobs[curr][2]);
         return dp[curr];
     }
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
         int n = startTime.size();
-        jobs.resize(n, vector<int>(3, 0));
+        N = n;
         for (int i = 0; i < n; ++i) {
-            jobs[i][0] = startTime[i];
-            jobs[i][1] = endTime[i];
-            jobs[i][2] = profit[i];
+            vector<int> job(3);
+            job[0] = startTime[i];
+            job[1] = endTime[i];
+            job[2] = profit[i];
+            jobs.push_back(job);
         }
         sort(jobs.begin(), jobs.end(), compare);
-        vector<int> start;
-        for (int i = 0; i < n; ++i) start.push_back(jobs[i][0]);
-        
-        dp.resize(n + 1, -1);
-        
-        return recursive(start, 0, n);
+        vector<int> start(n);
+        for (int i = 0; i < n; ++i) start[i] = jobs[i][0];
+        dp.resize(n, -1);
+        return recursive(start, 0);
     }
 };
