6d6c0a513019c86397704e30420a61f25a240778
Kaminyou <ikaminyou@gmail.com>
2022-06-24 01:36:24 +0800

    947v2

--- solutions/947.cpp
+++ solutions/947.cpp
@@ -30,3 +30,57 @@ public:
         return n - count;
     }
 };
+
+// class DisjointSet {
+// private:
+//     vector<int> parent;
+//     vector<int> rank;
+//     int N;
+// public:
+//     DisjointSet(int n) {
+//         parent.resize(n, 0);
+//         for (int i = 0; i < n; ++i) parent[i] = i;
+//         rank.resize(n, 0);
+//         N = n;
+//     }
+//     int find(int x) {
+//         if (x != parent[x]) {
+//             parent[x] = find(parent[x]);
+//         }
+//         return parent[x];
+//     }
+//     void join(int x, int y) {
+//         int pX = find(x);
+//         int pY = find(y);
+//         if (pX == pY) return;
+//         N--;
+//         if (rank[pX] > rank[pY]) {
+//             parent[pY] = parent[pX];
+//         }
+//         else if (rank[pX] < rank[pY]) {
+//             parent[pX] = parent[pY];
+//         }
+//         else {
+//             parent[pY] = parent[pX];
+//             rank[pX]++;
+//         }
+//     }
+//     int getSize() {
+//         return N;
+//     }
+// };
+// class Solution {
+// public:
+//     int removeStones(vector<vector<int>>& stones) {
+//         int n = stones.size();
+//         DisjointSet* disjointSet = new DisjointSet(n);
+//         for (int i = 0; i < n; ++i) {
+//             for (int j = i + 1; j < n; ++j) {
+//                 if (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
+//                     disjointSet->join(i, j);
+//                 }
+//             }
+//         }
+//         return n - disjointSet->getSize();
+//     }
+// };
