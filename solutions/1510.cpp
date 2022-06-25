0881309165ccd6c3436dddfeee03da7a282fede6
Kaminyou <ikaminyou@gmail.com>
2022-06-25 14:29:06 +0800

    1510v2

--- solutions/1510.cpp
+++ solutions/1510.cpp
@@ -2,9 +2,11 @@ class Solution {
 public:
     bool winnerSquareGame(int n) {
         vector<bool> dp(n + 1, false);
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j * j <= i; ++j) {
-                if (dp[i - j * j] == false) dp[i] = true;
+        for (int i = 0; i <= n; ++i) {
+            if (dp[i] == false) {
+                for (int j = 1; j * j + i <= n + 1; ++j) {
+                    dp[i + j * j] = true;
+                }
             }
         }
         return dp[n];
