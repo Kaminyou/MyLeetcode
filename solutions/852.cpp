0e26d6b31a8fcfa8934173c9a7ddc9b4f3b75967
Kaminyou <ikaminyou@gmail.com>
2022-06-26 03:02:11 +0800

    852

--- solutions/852.cpp
+++ solutions/852.cpp
@@ -16,3 +16,18 @@ public:
         return left;
     }
 };
+
+// v2
+// class Solution {
+// public:
+//     int peakIndexInMountainArray(vector<int>& A) {
+// 		int left = 0;
+//         int right = A.size() - 1;
+//         while (left < right) {
+//             int mid = left + (right - left) / 2;
+//             if (A[mid] > A[mid + 1]) right = mid;
+//             else left = mid + 1;
+//         } 
+//         return left;
+//     }
+// };
