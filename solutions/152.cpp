1fb89641fc467f0ae217a7acc35c7ce17cf39291
Kaminyou <ikaminyou@gmail.com>
2022-06-27 01:12:24 +0800

    152v2

--- solutions/152.cpp
+++ solutions/152.cpp
@@ -19,3 +19,34 @@ public:
         return maxVal;
     }
 };
+
+// v2
+// class Solution {
+// public:
+//     int maxProduct(vector<int>& nums) {
+//         int positive = 0;
+//         int negative = 0;
+//         int res = INT_MIN;
+//         for (auto& num : nums) {
+//             if (num == 0) {
+//                 positive = 0;
+//                 negative = 0;
+//                 res = max(res, 0);
+//             }
+//             else if (num > 0) {
+//                 positive = max(num, positive * num);
+//                 negative = min(num, negative * num);
+//                 res = max(res, positive);
+//             }
+//             else {
+//                 int temp = positive;
+//                 positive = max(0, negative * num);
+//                 if (positive > 0) res = max(res, positive);
+//                 negative = min(num, temp * num);
+//                 res = max(res, negative);
+//             }
+            
+//         }
+//         return res;
+//     }
+// };
