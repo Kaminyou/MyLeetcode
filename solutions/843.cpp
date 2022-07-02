2472228aad4cdca80e8adc3133a97d514aa5da7a
Kaminyou <ikaminyou@gmail.com>
2022-07-02 23:24:13 +0800

    843

---
+++ solutions/843.cpp
@@ -0,0 +1,45 @@
+/**
+ * // This is the Master's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class Master {
+ *   public:
+ *     int guess(string word);
+ * };
+ */
+class Solution {
+public:
+    int countMatch(const string& s1, const string& s2) {
+        int count = 0;
+        for (int i = 0; i < 6; ++i) {
+            if (s1[i] == s2[i]) count++;
+        }
+        return count;
+    }
+    void findSecretWord(vector<string>& wordlist, Master& master) {
+        sort(wordlist.begin(), wordlist.end());
+        unordered_map<string, unordered_map<int, unordered_set<string>>> mp;
+        int n = wordlist.size();
+        vector<int> available(n, true);
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                int count = countMatch(wordlist[i], wordlist[j]);
+                mp[wordlist[i]][count].insert(wordlist[j]);
+                mp[wordlist[j]][count].insert(wordlist[i]);
+            }
+        }
+        // guess
+        while (1) {
+            int rnd = rand() % wordlist.size();
+            string guess = wordlist[rnd];
+            int res = master.guess(guess);
+            if (res == 6) break;
+            vector<string> newWordList;
+            for (auto& word : wordlist) {
+                if (mp[guess][res].find(word) != mp[guess][res].end()) {
+                    newWordList.push_back(word);
+                }
+            }
+            wordlist = newWordList;
+        }
+    }
+};
