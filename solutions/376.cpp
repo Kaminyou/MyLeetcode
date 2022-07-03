e6843f9e94a8113f09630772cb259bcbae4af218
Kaminyou <ikaminyou@gmail.com>
2022-07-03 16:14:13 +0800

    376v2

--- solutions/376.cpp
+++ solutions/376.cpp
@@ -26,3 +26,28 @@ public:
         return maxLen;
     }
 };
+
+// v2
+// class Solution {
+// public:
+//     int wiggleMaxLength(vector<int>& nums) {
+//         vector<int> newNums;
+//         int index = 0;
+//         while (index < nums.size()) {
+//             int start = nums[index];
+//             while (index + 1 < nums.size() && nums[index + 1] == start) index++;
+//             newNums.push_back(nums[index]);
+//             index++;
+//         }
+//         nums = newNums;
+//         if (nums.size() == 1) return 1;
+//         int positive = 0;
+//         int negative = 0;
+//         int start = nums[0];
+//         for (int i = 1; i < nums.size() - 1; ++i) {
+//             if (nums[i - 1] < nums[i] && nums[i] > nums[i + 1]) positive++;
+//             if (nums[i - 1] > nums[i] && nums[i] < nums[i + 1]) negative++;
+//         }
+//         return positive + negative + 2;
+//     }
+// };
