c8ed3b07f05a7a4a40452e1968783be784f5d9ef
Kaminyou <ikaminyou@gmail.com>
2022-07-08 00:22:01 +0800

    97v2

--- solutions/97.cpp
+++ solutions/97.cpp
@@ -18,3 +18,31 @@ public:
         return dp[m][n];
     }
 };
+
+// V2
+// class Solution {
+// public:
+//     bool isInterleave(string s1, string s2, string s3) {
+//         int m = s1.size();
+//         int n = s2.size();
+//         int t = s3.size();
+//         if (m + n != t) return false;
+//         vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+//         dp[0][0] = true;
+        
+//         for (int i = 1; i <= m; ++i) {
+//             if (s1[i - 1] == s3[i - 1] && dp[i - 1][0]) dp[i][0] = true;
+//         }
+//         for (int j = 1; j <= n; ++j) {
+//             if (s2[j - 1] == s3[j - 1] && dp[0][j - 1]) dp[0][j] = true;
+//         }
+//         for (int i = 1; i <= m; ++i) {
+//             for (int j = 1; j <= n; ++j) {
+//                 int target = i + j - 1;
+//                 if (s3[target] == s1[i - 1] && dp[i - 1][j]) dp[i][j] = true;
+//                 if (s3[target] == s2[j - 1] && dp[i][j - 1]) dp[i][j] = true;
+//             }
+//         }
+//         return dp[m][n];
+//     }
+// };
