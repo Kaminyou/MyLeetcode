b123190e5a77f78bc1f60e9cfcfdf71753759846
Kaminyou <ikaminyou@gmail.com>
2022-07-11 01:01:09 +0800

    329v2

--- solutions/329.cpp
+++ solutions/329.cpp
@@ -50,3 +50,36 @@ public:
         return level;
     }
 };
+
+// V2: DFS
+// class Solution {
+// public:
+//     int M;
+//     int N;
+//     vector<pair<int, int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+//     int dfs(int x, int y, vector<vector<int>>& matrix, vector<vector<int>>& memo) {
+//         if (memo[x][y] != -1) return memo[x][y];
+//         int res = 1;
+//         for (auto& direction : directions) {
+//             int nextX = x + direction.first;
+//             int nextY = y + direction.second;
+//             if (nextX < 0 || nextX >= M || nextY < 0 || nextY >= N) continue;
+//             if (matrix[nextX][nextY] <= matrix[x][y]) continue;
+//             res = max(res, 1 + dfs(nextX, nextY, matrix, memo));
+//         }
+//         return memo[x][y] = res;
+//     }
+//     int longestIncreasingPath(vector<vector<int>>& matrix) {
+//         M = matrix.size();
+//         N = matrix[0].size();
+//         vector<vector<int>> memo(M, vector<int>(N, -1));
+//         int res = 0;
+//         for (int i = 0; i < M; ++i) {
+//             for (int j = 0; j < N; ++j) {
+//                 int length = dfs(i, j, matrix, memo);
+//                 res = max(res, length);
+//             }
+//         }
+//         return res;
+//     }
+// };
