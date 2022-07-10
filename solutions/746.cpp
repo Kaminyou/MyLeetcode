6ea7221def84c95903887d275e6e905a630d14c1
Kaminyou <ikaminyou@gmail.com>
2022-07-10 17:22:53 +0800

    746v2

--- solutions/746.cpp
+++ solutions/746.cpp
@@ -5,9 +5,8 @@ public:
         vector<int> dp(n + 1, 0);
         dp[0] = 0;
         dp[1] = 0;
-        dp[2] = min(cost[0], cost[1]);
-        for (int i = 3; i <= n; i++) {
-            dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        for (int i = 2; i <= n; ++i) {
+            dp[i] = min(dp[i - 2] + cost[i - 2], dp[i - 1] + cost[i - 1]);
         }
         return dp[n];
     }
