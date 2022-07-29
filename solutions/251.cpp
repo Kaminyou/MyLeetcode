a20337a056ffb17e755af07bc6299701ededef7d
Kaminyou <ikaminyou@gmail.com>
2022-07-29 21:14:08 +0800

    251v2

--- solutions/251.cpp
+++ solutions/251.cpp
@@ -35,3 +35,54 @@ public:
  * int param_1 = obj->next();
  * bool param_2 = obj->hasNext();
  */
+
+// V2
+// class Vector2D {
+// public:
+//     vector<vector<int>>* vec;
+//     int row = 0;
+//     int col = -1;
+//     Vector2D(vector<vector<int>>& vec) {
+//         this->vec = &vec;
+//         findNext();
+//     }
+//     void findNext() {
+//         if (row == (*vec).size()) {
+//             row = -1;
+//             col = -1;
+//             return;
+//         }
+//         while (true) {
+//             if ((*vec)[row].size() != col + 1) {
+//                 col++;
+//                 break;
+//             }
+//             else {
+//                 row++;
+//                 col = -1;
+//                 if (row == (*vec).size()) {
+//                     row = -1;
+//                     col = -1;
+//                     break;
+//                 }
+//             }
+//         }
+//     }
+//     int next() {
+//         int value = (*vec)[row][col];
+//         findNext();
+//         return value;
+//     }
+    
+//     bool hasNext() {
+//         if (row != -1 && col != -1) return true;
+//         return false;
+//     }
+// };
+
+// /**
+//  * Your Vector2D object will be instantiated and called as such:
+//  * Vector2D* obj = new Vector2D(vec);
+//  * int param_1 = obj->next();
+//  * bool param_2 = obj->hasNext();
+//  */
