9a57cdbb35e74b4b9a97b47bfd1ba04057ae01fd
Kaminyou <ikaminyou@gmail.com>
2022-08-02 22:44:13 +0800

    378v2

--- solutions/378.cpp
+++ solutions/378.cpp
@@ -18,3 +18,26 @@ public:
         return left;
     }
 };
+
+// v2
+// typedef pair<int, pair<int, int>> P; // num {x, y}
+// class Solution {
+// public:
+//     int kthSmallest(vector<vector<int>>& matrix, int k) {
+//         int n = matrix.size();
+//         priority_queue<P, vector<P>, greater<P>> pq;
+//         for (int i = 0; i < n; ++i) {
+//             pq.push(make_pair(matrix[i][0], make_pair(i, 0)));
+//         }
+        
+//         int ans = -1;
+//         while (k--) {
+//             auto [num, idx] = pq.top();
+//             auto [x, y] = idx;
+//             ans = num;
+//             pq.pop();
+//             if (y + 1 != n) pq.push(make_pair(matrix[x][y + 1], make_pair(x, y + 1)));
+//         }
+//         return ans;
+//     }
+// };
