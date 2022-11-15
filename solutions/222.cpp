ebdd3441066bb46e40cc85841c60b7e05bf918ed
Kaminyou <ikaminyou@gmail.com>
2022-11-15 19:21:32 +0800

    222v2

--- solutions/222.cpp
+++ solutions/222.cpp
@@ -9,6 +9,43 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int getLeftHeight(TreeNode* node) {
+        int height = 0;
+        while (node) {
+            node = node->left;
+            height++;
+        }
+        return height;
+    }
+    int getRightHeight(TreeNode* node) {
+        int height = 0;
+        while (node) {
+            node = node->right;
+            height++;
+        }
+        return height;
+    }
+    int countNodes(TreeNode* root) {
+        if (!root) return 0;
+        int leftHeight = getLeftHeight(root->left);
+        int rightHeight = getRightHeight(root->right);
+        if (leftHeight == rightHeight) return (1 << (leftHeight + 1)) - 1;
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+};
 class Solution {
 public:
     int getHeight(TreeNode* node) {
