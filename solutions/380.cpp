d9cc559e4551ff057f2400d09602b8d53382605e
Kaminyou <ikaminyou@gmail.com>
2022-11-30 01:45:06 +0800

    380v2

--- solutions/380.cpp
+++ solutions/380.cpp
@@ -41,3 +41,46 @@ public:
  * bool param_2 = obj->remove(val);
  * int param_3 = obj->getRandom();
  */
+
+ class RandomizedSet {
+public:
+    unordered_map<int, int> mp;
+    vector<int> v;
+    int index = 0;
+    RandomizedSet() {
+        
+    }
+    
+    bool insert(int val) {
+        if (mp.count(val)) return false;
+        mp[val] = index;
+        index++;
+        v.push_back(val);
+        return true;
+    }
+    
+    bool remove(int val) {
+        if (!mp.count(val)) return false;
+        int removeIdx = mp[val];
+        index--;
+        int exchangeVal = v[index];
+        mp[exchangeVal] = removeIdx;
+        swap(v[removeIdx], v[index]);
+        v.pop_back();
+        mp.erase(val);
+        return true;
+    }
+    
+    int getRandom() {
+        int rndIdx = rand() % index;
+        return v[rndIdx];
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
