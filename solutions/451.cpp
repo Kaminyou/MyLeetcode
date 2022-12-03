1ef47c3ca32e2f1b614fcd8d7b8f0758fe5a2d00
Kaminyou <ikaminyou@gmail.com>
2022-12-03 22:03:40 +0800

    451v2

--- solutions/451.cpp
+++ solutions/451.cpp
@@ -20,3 +20,36 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    int char2idx(char c) {
+        if (c >= 'a' && c <= 'z') return c - 'a';
+        if (c >= 'A' && c <= 'Z') return c - 'A' + 26;
+        return c - '0' + 52;
+    }
+    static bool compare(pair<int, char>& p1, pair<int, char>& p2) {
+        return p1.first > p2.first;
+    }
+    string frequencySort(string s) {
+        vector<pair<int, char>> counts;
+        for (int i = 0; i < 26; ++i) {
+            counts.push_back({0, i + 'a'});
+        }
+        for (int i = 0; i < 26; ++i) {
+            counts.push_back({0, i + 'A'});
+        }
+        for (int i = 0; i < 10; ++i) {
+            counts.push_back({0, i + '0'});
+        }
+        for (auto& c : s) {
+            counts[char2idx(c)].first++;
+        }
+        sort(counts.begin(), counts.end(), compare);
+        string res;
+        for (int i = 0; i < 62; ++i) {
+            res += string(counts[i].first, counts[i].second);
+        }
+        return res;
+    }
+};
