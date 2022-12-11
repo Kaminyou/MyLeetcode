84a18510839f63ee2377cd0f81b4a88a52dd59e1
Kaminyou <ikaminyou@gmail.com>
2022-12-11 11:46:50 +0800

    124v2

--- solutions/124.cpp
+++ solutions/124.cpp
@@ -28,3 +28,31 @@ public:
         return res;
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int res = INT_MIN;
+    int dfs(TreeNode* node) {
+        if (!node) return 0;
+        int left = dfs(node->left);
+        int right = dfs(node->right);
+        int current = max({node->val, left + node->val, right + node->val});
+        res = max({res, current, left + right + node->val});
+        return current;
+    }
+    int maxPathSum(TreeNode* root) {
+        dfs(root);
+        return res;
+    }
+};
