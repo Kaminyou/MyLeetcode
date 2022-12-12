efd016841c58ab7cb8f76947c331378f6e043d7e
Kaminyou <ikaminyou@gmail.com>
2022-12-12 20:58:39 +0800

    70v2

--- solutions/70.cpp
+++ solutions/70.cpp
@@ -13,3 +13,19 @@ public:
         return dp[n];
     }
 };
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        int prevTwo = 1;
+        int prevOne = 2;
+        for (int i = 2; i < n; ++i) {
+            int current = prevTwo + prevOne;
+            prevTwo = prevOne;
+            prevOne = current;
+        }
+        return prevOne;
+    }
+};
