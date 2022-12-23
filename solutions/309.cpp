b8e9dc43ad1cf9ce66a9a11a5471957eb0ccc74e
Kaminyou <ikaminyou@gmail.com>
2022-12-24 01:53:15 +0800

    309v2

--- solutions/309.cpp
+++ solutions/309.cpp
@@ -17,3 +17,27 @@ public:
         return max(sell, cooldown);
     }
 };
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<int> bought(n, 0);
+        vector<int> sold(n, 0);
+        
+        for (int i = 0; i < n; ++i) {
+            if (i >= 2) {
+                bought[i] = max({bought[i - 1], -prices[i], sold[i - 2] - prices[i]});
+            }
+            else if (i >= 1) {
+                bought[i] = max({bought[i - 1], -prices[i]});
+            }
+            else bought[i] = -prices[i];
+            if (i >= 1) {
+                sold[i] = max(sold[i - 1], bought[i - 1] + prices[i]);
+            }
+            else sold[i] = 0;
+        }
+        return sold[n - 1];
+    }
+};
