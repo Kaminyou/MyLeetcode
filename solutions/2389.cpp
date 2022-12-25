42e4ecc1df88202e4b185e458cd27e614399cf1c
Kaminyou <ikaminyou@gmail.com>
2022-12-25 15:52:58 +0800

    2389v2

--- solutions/2389.cpp
+++ solutions/2389.cpp
@@ -20,3 +20,31 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        int m = queries.size();
+        vector<int> res(m, 0);
+        sort(nums.begin(), nums.end());
+        
+        int n = nums.size();
+        vector<pair<int, int>> queriesWithIdx(m, {0, 0});
+        for (int i = 0; i < m; ++i) {
+            queriesWithIdx[i].first = queries[i];
+            queriesWithIdx[i].second = i;
+        }
+        sort(queriesWithIdx.begin(), queriesWithIdx.end());
+
+        int index = 0;
+        int sum = 0;
+        for (auto& [query, idx] : queriesWithIdx) {
+            while (index < n && sum + nums[index] <= query) {
+                sum += nums[index];
+                index++;
+            }
+            res[idx] = index;
+        }
+        return res;
+    }
+};
