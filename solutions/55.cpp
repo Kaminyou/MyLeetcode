eacf9df189f800dbb1df173e070ea2c5de673892
Kaminyou <ikaminyou@gmail.com>
2022-12-26 22:04:39 +0800

    55v2

--- solutions/55.cpp
+++ solutions/55.cpp
@@ -9,3 +9,16 @@ public:
         return far == 0;
     }
 };
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int farest = 0;
+        int n = nums.size();
+        for (int i = 0; i < n - 1; ++i) {
+            farest = max(farest, i + nums[i]);
+            if (farest < i + 1) return false;
+        }
+        return true;
+    }
+};
