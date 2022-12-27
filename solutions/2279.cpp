0ce8850d00c109fd6eb52412b083c8a6dd8c2eb0
Kaminyou <ikaminyou@gmail.com>
2022-12-27 19:32:50 +0800

    2279v2

--- solutions/2279.cpp
+++ solutions/2279.cpp
@@ -20,3 +20,23 @@ public:
         return count;
     }
 };
+
+class Solution {
+public:
+    int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
+        int n = capacity.size();
+        vector<int> left(n, 0);
+        for (int i = 0; i < n; ++i) {
+            left[i] = capacity[i] - rocks[i];
+        }
+        sort(left.begin(), left.end());
+        int cnt = 0;
+        for (int i = 0; i < n && additionalRocks > 0; ++i) {
+            int temp = min(left[i], additionalRocks);
+            left[i] -= temp;
+            additionalRocks -= temp;
+            if (left[i] == 0) cnt++;
+        }
+        return cnt;
+    }
+};
