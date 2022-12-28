5b410e4befc0dc38356421b62dc7109d08b38011
Kaminyou <ikaminyou@gmail.com>
2022-12-28 20:57:52 +0800

    1962v2

--- solutions/1962.cpp
+++ solutions/1962.cpp
@@ -13,3 +13,23 @@ public:
         return sum;
     }
 };
+
+class Solution {
+public:
+    int minStoneSum(vector<int>& piles, int k) {
+        priority_queue<int> pq;
+        int sum = 0;
+        for (auto& pile : piles) {
+            sum += pile;
+            pq.push(pile);
+        }
+        while (k--) {
+            int maxOne = pq.top();
+            pq.pop();
+            int remove = maxOne / 2;
+            sum -= remove;
+            pq.push(maxOne - remove);
+        }
+        return sum;
+    }
+};
