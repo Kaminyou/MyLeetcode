a8207eff8871973623165ecc382b81675da284eb
Kaminyou <ikaminyou@gmail.com>
2022-12-30 23:50:58 +0800

    797v2

--- solutions/797.cpp
+++ solutions/797.cpp
@@ -19,3 +19,27 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    vector<vector<int>> paths;
+    void backtracking(int parent, int node, int target, vector<int>& path, vector<vector<int>>& graph) {
+        if (node == target) {
+            paths.push_back(path);
+            return;
+        }
+        for (auto& neighbor : graph[node]) {
+            if (neighbor == parent) continue;
+            path.push_back(neighbor);
+            backtracking(node, neighbor, target, path, graph);
+            path.pop_back();
+        }
+    }
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> path;
+        path.push_back(0);
+        backtracking(-1, 0, n - 1, path, graph);
+        return paths;
+    }
+};
