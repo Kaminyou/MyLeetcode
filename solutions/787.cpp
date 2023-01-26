b2e29fe5f1973d98194476a4c57b6c365956b2cb
Kaminyou <ikaminyou@gmail.com>
2023-01-26 12:00:48 +0800

    787v2

--- solutions/787.cpp
+++ solutions/787.cpp
@@ -13,3 +13,18 @@ public:
         return distances[k + 1][dst] == INF ? -1 : distances[k + 1][dst];
     }
 };
+
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<int>> dp(k + 2, vector<int>(n, INT_MAX / 2));
+        dp[0][src] = 0;
+        for (int i = 1; i <= k + 1; ++i) {
+            dp[i][src] = 0;
+            for (auto& flight : flights) {
+                dp[i][flight[1]] = min(dp[i][flight[1]], dp[i - 1][flight[0]] + flight[2]);
+            }
+        }
+        return dp[k + 1][dst] >= INT_MAX / 2 ? -1 : dp[k + 1][dst];
+    }
+};
