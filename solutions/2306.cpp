4cd63ae2691fa5c23af9dd4a7de2cfa6e3c7aece
Kaminyou <ikaminyou@gmail.com>
2023-02-09 19:57:43 +0800

    2306v2

--- solutions/2306.cpp
+++ solutions/2306.cpp
@@ -24,3 +24,27 @@ public:
         return res;
     }
 };
+
+// v2
+class Solution {
+public:
+    long long distinctNames(vector<string>& ideas) {
+        unordered_map<char, unordered_set<string>> mp;
+        for (auto& idea : ideas) {
+            mp[idea[0]].insert(idea.substr(1));
+        }
+        long long res = 0;
+        for (char i = 'a'; i <= 'z'; ++i) {
+            for (char j = i + 1; j <= 'z'; ++j) {
+                int m = mp[i].size();
+                int n = mp[j].size();
+                int intersect = 0;
+                for (auto& suffix : mp[j]) {
+                    if (mp[i].count(suffix)) intersect++;
+                }
+                res += (long long)(m - intersect) * (long long)(n - intersect) * 2;
+            }
+        }
+        return res;
+    }
+};
