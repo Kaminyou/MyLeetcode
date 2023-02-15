46a7b039117aeb5b18d029f9c8fc4c0959595b02
Kaminyou <ikaminyou@gmail.com>
2023-02-15 23:13:37 +0800

    989

--- solutions/989.cpp
+++ solutions/989.cpp
@@ -37,3 +37,38 @@ public:
         return res;
     }
 };
+
+// v2
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int>& num, int k) {
+        vector<int> kNum;
+        while (k) {
+            kNum.push_back(k % 10);
+            k /= 10;
+        }
+        reverse(num.begin(), num.end());
+        int numIdx = 0;
+        int kNumIdx = 0;
+        int carry = 0;
+        vector<int> res;
+        while (numIdx < num.size() || kNumIdx < kNum.size()) {
+            if (numIdx < num.size()) {
+                carry += num[numIdx];
+                numIdx++;
+            }
+            if (kNumIdx < kNum.size()) {
+                carry += kNum[kNumIdx];
+                kNumIdx++;
+            }
+            res.push_back(carry % 10);
+            carry /= 10;
+        }
+        while (carry) {
+            res.push_back(carry % 10);
+            carry /= 10;
+        }
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
