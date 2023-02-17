9c4261b2bb153398b9cbac8943e9c62399feb5c5
Kaminyou <ikaminyou@gmail.com>
2023-02-18 00:53:39 +0800

    783

--- solutions/783.cpp
+++ solutions/783.cpp
@@ -29,3 +29,32 @@ public:
         return res;
     }
 };
+
+// v2
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int res = INT_MAX;
+    int minVal = INT_MIN / 2;
+    void preorder(TreeNode* node) {
+        if (!node) return;
+        preorder(node->left);
+        res = min(res, node->val - minVal);
+        minVal = node->val;
+        preorder(node->right);
+    }
+    int minDiffInBST(TreeNode* root) {
+        preorder(root);
+        return res;
+    }
+};
