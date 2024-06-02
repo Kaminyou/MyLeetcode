c3c3e24abe628c43b6a2794212100b754fb8185f
Kaminyou <ikaminyou@gmail.com>
2024-06-02 10:31:39 +0800

    344

---
+++ rust_solutions/344.rs
@@ -0,0 +1,11 @@
+impl Solution {
+    pub fn reverse_string(s: &mut Vec<char>) {
+        let mut left = 0;
+        let mut right = s.len() - 1;
+        while left < right {
+            s.swap(left, right);
+            left += 1;
+            right -= 1;
+        }
+    }
+}
