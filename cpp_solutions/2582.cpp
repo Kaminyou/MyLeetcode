2bd30c3ad5cef8686d6e0ea1269b6112afe98b06
Kaminyou <ikaminyou@gmail.com>
2024-07-06 11:28:42 +0800

    2482

--- cpp_solutions/2582.cpp
+++ cpp_solutions/2582.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int passThePillow(int n, int time) {
-        int span = n * 2 - 2;
-        time %= span;
-        if (time <= (n - 1)) return time + 1;
-        time -= (n - 1);
-        return n - time;
+        int mod = 2 * n - 2;
+        int residual = time % mod;
+        if (residual >= n) residual = mod - residual;
+        return residual + 1;
     }
 };
+
