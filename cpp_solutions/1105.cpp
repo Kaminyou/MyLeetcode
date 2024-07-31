7d0cd807a83bb71e8a8dc91a7ff48892e8399351
Kaminyou <ikaminyou@gmail.com>
2024-07-31 21:01:03 +0800

    1105

--- cpp_solutions/1105.cpp
+++ cpp_solutions/1105.cpp
@@ -32,3 +32,25 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
+        int n = books.size();
+        books.insert(books.begin(), {0, 0});
+
+        vector<int> dp(n + 1, INT_MAX);
+        dp[0] = 0;
+        for (int i = 1; i <= n; ++i) {
+            int maxHeight = 0;
+            int width = 0;
+            for (int j = i; j >= 1; --j) {
+                maxHeight = max(maxHeight, books[j][1]);
+                width += books[j][0];
+                if (width > shelfWidth) break;
+                dp[i] = min(dp[i], dp[j - 1] + maxHeight);
+            }
+        }
+        return dp[n];
+    }
+};
