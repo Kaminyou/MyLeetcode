7ae0cd0668f4a93cc371ae688b5d4c2e506c38e8
Kaminyou <ikaminyou@gmail.com>
2024-09-17 09:31:17 +0900

    update 884

--- cpp_solutions/884.cpp
+++ cpp_solutions/884.cpp
@@ -2,28 +2,22 @@ class Solution {
 public:
     vector<string> uncommonFromSentences(string s1, string s2) {
         unordered_map<string, int> mp1;
+        unordered_map<string, int> mp2;
+        stringstream ss1(s1);
+        stringstream ss2(s2);
         string token;
-        stringstream ss(s1);
-        while (getline(ss, token, ' ')) {
+        while (getline(ss1, token, ' ')) {
             mp1[token]++;
         }
-        
-        unordered_map<string, int> mp2;
-        stringstream ss2(s2);
         while (getline(ss2, token, ' ')) {
-            if (mp1.find(token) != mp1.end()) mp1[token] = 2;
-            else {
-                mp2[token]++;
-            }
+            mp2[token]++;
         }
         vector<string> res;
-        for (auto& element : mp1) {
-            if (element.second != 1) continue;
-            res.push_back(element.first);
+        for (auto& [s, num] : mp1) {
+            if (num == 1 && mp2.count(s) == 0) res.push_back(s);
         }
-        for (auto& element : mp2) {
-            if (element.second != 1) continue;
-            res.push_back(element.first);
+        for (auto& [s, num] : mp2) {
+            if (num == 1 && mp1.count(s) == 0) res.push_back(s);
         }
         return res;
     }
