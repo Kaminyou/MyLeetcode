02ff956b450b7cdfb0ce6036fe2e1b2942184570
Kaminyou <ikaminyou@gmail.com>
2024-10-23 11:48:20 +0900

    update 2641

--- cpp_solutions/2641.cpp
+++ cpp_solutions/2641.cpp
@@ -11,30 +11,48 @@
  */
 class Solution {
 public:
-    void dfs(TreeNode* node, int level, unordered_map<int, int>& levelSum) {
-        if (!node) return;
-        levelSum[level] += node->val;
-        dfs(node->left, level + 1, levelSum);
-        dfs(node->right, level + 1, levelSum);
-    }
-    void dfs2(TreeNode* node, int level, unordered_map<int, int>& levelSum) {
-        if (!node) return;
-        int leftVal = 0;
-        int rightVal = 0;
-        if (node->left) leftVal = node->left->val;
-        if (node->right) rightVal = node->right->val;
-        
-        int valForNextLevel = levelSum[level + 1] - leftVal - rightVal;
-        if (node->left) node->left->val = valForNextLevel;
-        if (node->right) node->right->val = valForNextLevel;
-        dfs2(node->left, level + 1, levelSum);
-        dfs2(node->right, level + 1, levelSum);
-    }
     TreeNode* replaceValueInTree(TreeNode* root) {
-        unordered_map<int, int> levelSum;
-        dfs(root, 0, levelSum);
-        if (root) root->val = 0;
-        dfs2(root, 0, levelSum);
+        queue<TreeNode*> q;
+        queue<TreeNode*> tempQ;
+        if (root) {
+            q.push(root);
+            root->val = 0;
+        }
+        while (!q.empty()) {
+            int nextSum = 0;
+            int n = q.size();
+            for (int i = 0; i < n; ++i) {
+                TreeNode* node = q.front();
+                q.pop();
+                tempQ.push(node);
+                if (node->left) {
+                    nextSum += node->left->val;
+                    q.push(node->left);
+                }
+                if (node->right) {
+                    nextSum += node->right->val;
+                    q.push(node->right);
+                }
+            }
+            while (!tempQ.empty()) {
+                TreeNode* node = tempQ.front();
+                tempQ.pop();
+                int nextVal = nextSum;
+                if (node->left) {
+                    nextVal -= node->left->val;
+                }
+                if (node->right) {
+                    nextVal -= node->right->val;
+                }
+                if (node->left) {
+                    node->left->val = nextVal;
+                }
+                if (node->right) {
+                    node->right->val = nextVal;
+                }
+            }
+        }
         return root;
     }
 };
+
