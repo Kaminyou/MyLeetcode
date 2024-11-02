b662bd1f11769c274db5f8971b2a806b5c6a2444
Kaminyou <ikaminyou@gmail.com>
2024-11-02 13:13:44 +0900

    update 2490

--- cpp_solutions/2490.cpp
+++ cpp_solutions/2490.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     bool isCircularSentence(string sentence) {
-        vector<string> words;
-        string temp;
+        int n = sentence.size();
+        char c = sentence[n - 1];
+        string token;
         stringstream ss(sentence);
-        while (getline(ss, temp, ' ')) {
-            words.push_back(temp);
-        }
-        words.push_back(words[0]);
-        int n = words.size();
-        for (int i = 0; i < n - 1; ++i) {
-            if (words[i].back() != words[i + 1].front()) return false;
+        while (getline(ss, token, ' ')) {
+            int m = token.size();
+            char target = token[0];
+            if (target != c) return false;
+            c = token[m - 1];
         }
         return true;
     }
