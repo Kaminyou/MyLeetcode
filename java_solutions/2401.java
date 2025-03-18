1275dd9b4f38379e5c3b4c50ed90718856b38ef3
Kaminyou <ikaminyou@gmail.com>
2025-03-18 21:00:50 +0800

    add 2401

--- java_solutions/2401.java
+++ java_solutions/2401.java
@@ -1,18 +1,17 @@
 class Solution {
-public:
-    int longestNiceSubarray(vector<int>& nums) {
+    public int longestNiceSubarray(int[] nums) {
         int res = 1;
         int right = 0;
         int now = 0;
-        int n = nums.size();
-        for (int left = 0; left < n; ++left) {
+        int n = nums.length;
+        for (int left = 0; left < n; left += 1) {
             while (right < n && ((nums[right] & now) == 0)) {
                 now |= nums[right];
                 right++;
             }
-            res = max(res, right - left);
+            res = Integer.max(res, right - left);
             now ^= nums[left];
         }
         return res;
     }
-};
+}
