bbd4f772d1184bef43516c1cccc0e511c7f49c57
Kaminyou <ikaminyou@gmail.com>
2025-04-19 11:54:40 +0800

    2563

--- cpp_solutions/2563.cpp
+++ cpp_solutions/2563.cpp
@@ -20,3 +20,30 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        long long res = 0;
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int rightUpper = n - 1;
+        int rightLower = n;
+        for (int left = 0; left < n; ++left) {
+            while (rightLower > 0 && nums[left] + nums[rightLower - 1] >= lower) {
+                rightLower--;
+            }
+            while (rightUpper > 0 && nums[left] + nums[rightUpper] > upper) {
+                rightUpper--;
+            }
+            int usedRightLower = max(left + 1, rightLower);
+            if (usedRightLower == n) continue;
+            if (nums[left] + nums[usedRightLower] < lower) continue;
+            if (nums[left] + nums[rightUpper] > upper) continue;
+            if (usedRightLower > rightUpper) continue;
+            res += (rightUpper - usedRightLower + 1);
+        }
+        return res;
+    }
+};
+
