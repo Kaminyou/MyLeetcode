68418ee5250ede75ee2148fdd73a1c7ccd93caed
Kaminyou <ikaminyou@gmail.com>
2025-06-26 21:53:13 +0800

    2311v2

--- cpp_solutions/2311.cpp
+++ cpp_solutions/2311.cpp
@@ -17,3 +17,35 @@ public:
         return count;
     }
 };
+
+class Solution {
+public:
+    bool check(string s, string t, int indexS, int indexT) {
+        if (indexS == s.size()) return true;
+        if (s[indexS] < t[indexT]) return true;
+        if (s[indexS] > t[indexT]) return false;
+        return check(s, t, indexS + 1, indexT + 1);
+    }
+    int longestSubsequence(string s, int k) {
+        string t = "";
+        while (k) {
+            if (k & 1) t.push_back('1');
+            else t.push_back('0');
+            k /= 2;
+        }
+        reverse(t.begin(), t.end());
+        int m = s.size();
+        int n = t.size();
+        if (n > m) return m;
+        int kk = m - n;
+        int zeros = 0;
+        for (int i = 0; i < kk; ++i) {
+            if (s[i] == '0') zeros++;
+        }
+        if (check(s, t, kk, 0)) {
+            return n + zeros;
+        }
+        return n + zeros - 1;
+    }
+};
+
