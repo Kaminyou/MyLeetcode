4b1388c24e10a646b5e215805982c433b3439e46
Kaminyou <ikaminyou@gmail.com>
2025-09-07 00:35:03 -0400

    1304v2

--- cpp_solutions/1304.cpp
+++ cpp_solutions/1304.cpp
@@ -12,3 +12,16 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        vector<int> res;
+        for (int i = 1; i <= n / 2; ++i) {
+            res.push_back(i);
+            res.push_back(-i);
+        }
+        if (n & 1) res.push_back(0);
+        return res;
+    }
+};
