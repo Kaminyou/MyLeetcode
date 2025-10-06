51d465e063079017d5dc41ff3eebc4642d5a1167
Kaminyou <ikaminyou@gmail.com>
2025-10-05 21:24:09 -0400

    778v2

--- cpp_solutions/778.cpp
+++ cpp_solutions/778.cpp
@@ -31,3 +31,78 @@ public:
         return -1;
     }
 };
+
+class DisjointSet {
+private:
+    vector<int> parents;
+    vector<int> ranks;
+public:
+    DisjointSet(int n) {
+        parents.resize(n, 0);
+        for (int i = 0; i < n; ++i) {
+            parents[i] = i;
+        }
+        ranks.resize(n, 0);
+    }
+    int find(int x) {
+        if (x != parents[x]) {
+            parents[x] = find(parents[x]);
+        }
+        return parents[x];
+    }
+    void join(int x, int y) {
+        int pX = find(x);
+        int pY = find(y);
+        if (pX == pY) return;
+        if (ranks[pX] >= ranks[pY]) {
+            parents[pY] = pX;
+            ranks[pX]++;
+        }
+        else {
+            parents[pX] = pY;
+            ranks[pY]++;
+        }
+    }
+};
+class Solution {
+public:
+    int N;
+    vector<pair<int, int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    int grid2index(int x, int y) {
+        return x * N + y;
+    }
+    pair<int, int> index2grid(int index) {
+        int x = index / N;
+        int y = index % N;
+        return {x, y};
+    }
+    int swimInWater(vector<vector<int>>& grid) {
+        int n = grid.size();
+        N = n;
+        DisjointSet* disjointSet = new DisjointSet(n * n);
+        vector<pair<int, int>> v;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int value = grid[i][j];
+                int index = grid2index(i, j);
+                v.push_back({value, index});
+            }
+        }
+        sort(v.begin(), v.end());
+        for (int i = 0; i < n * n; ++i) {
+            int value = v[i].first;
+            int index = v[i].second;
+            pair<int, int> p = index2grid(index);
+            for (auto& direction : directions) {
+                int xx = direction.first + p.first;
+                int yy = direction.second + p.second;
+                if (xx < 0 || xx >= n || yy < 0 || yy >= n) continue;
+                if (grid[xx][yy] > grid[p.first][p.second]) continue;
+                int nextIndex = grid2index(xx, yy);
+                disjointSet->join(index, nextIndex);
+            }
+            if (disjointSet->find(0) == disjointSet->find(n * n - 1)) return value;
+        }
+        return -1;
+    }
+};
