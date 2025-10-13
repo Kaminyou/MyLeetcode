a342036cf71a3e3b2d72b6a97acad9f0120ef305
Kaminyou <ikaminyou@gmail.com>
2025-10-12 20:38:12 -0400

    2273v2

--- cpp_solutions/2273.cpp
+++ cpp_solutions/2273.cpp
@@ -13,3 +13,29 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    int counts[26];
+    bool isSame(string& a, string& b) {
+        for (int i = 0; i < 26; ++i) counts[i] = 0;
+        for (auto& c : a) counts[c - 'a']++;
+        for (auto& c : b) counts[c - 'a']--;
+        for (int i = 0; i < 26; ++i) {
+            if (counts[i] != 0) return false;
+        }
+        return true;
+    }
+    vector<string> removeAnagrams(vector<string>& words) {
+        int n = words.size();
+        int index = 0;
+        vector<string> res;
+        while (index < n) {
+            int left = index;
+            while (index + 1 < n && isSame(words[index], words[index + 1])) index++;
+            res.push_back(words[left]);
+            index++;
+        }
+        return res;
+    }
+};
