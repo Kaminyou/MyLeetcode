050e92c5073f1b20644d904e06d322077994cc6d
Kaminyou <ikaminyou@gmail.com>
2026-01-01 21:19:07 -0500

    961v2

--- cpp_solutions/961.cpp
+++ cpp_solutions/961.cpp
@@ -9,3 +9,14 @@ public:
         return -1;
     }
 };
+
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 2; i < n; ++i) {
+            if (nums[i] == nums[i - 1] || nums[i] == nums[i - 2]) return nums[i];
+        }
+        return nums[0];
+    }
+};
