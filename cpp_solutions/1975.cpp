dab864f69fafac731512729d5c8911e3d56f0775
Kaminyou <ikaminyou@gmail.com>
2026-01-04 21:15:01 -0500

    1975 update

--- cpp_solutions/1975.cpp
+++ cpp_solutions/1975.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     long long maxMatrixSum(vector<vector<int>>& matrix) {
         long long res = 0;
-        int close = INT_MAX;
-        int cnt = 0;
+        int negCnt = 0;
+        long long close = INT_MAX;
         int n = matrix.size();
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < n; ++j) {
                 res += abs(matrix[i][j]);
-                if (matrix[i][j] < 0) cnt++;
-                if (abs(matrix[i][j]) < close) close = abs(matrix[i][j]);
+                if (matrix[i][j] < 0) negCnt++;
+                close = min(close, (long long)abs(matrix[i][j]));
             }
         }
-        if (!(cnt & 1)) return res;
-        return res - close * 2;
+        if (!(negCnt & 1)) return res;
+        return res - 2 * close;
     }
 };
-
