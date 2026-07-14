7a26d54f306263aed8f998a8e8ae06f26e9201be
Kaminyou <ikaminyou@gmail.com>
2026-07-13 22:42:44 -0400

    3336

--- cpp_solutions/3336.cpp
+++ cpp_solutions/3336.cpp
@@ -4,20 +4,31 @@ public:
         int M = *max_element(nums.begin(), nums.end());
         int mod = 1e9 + 7;
         vector<vector<int>> dp(M + 1, vector<int>(M + 1, 0));
+        vector<vector<int>> tempDP(M + 1, vector<int>(M + 1, 0));
         dp[0][0] = 1;
         for (auto& num : nums) {
-            vector<vector<int>> temp(M + 1, vector<int>(M + 1, 0));
+            // copy values
+            for (int i = M; i >= 0; --i) {
+                for (int j = M; j >= 0; --j) {
+                    tempDP[i][j] = dp[i][j];
+                }
+            }
+            // update
             for (int i = M; i >= 0; --i) {
                 for (int j = M; j >= 0; --j) {
                     int i2 = __gcd(num, i);
                     int j2 = __gcd(num, j);
                     int v = dp[i][j];
-                    temp[i2][j] = (temp[i2][j] + v) % mod;
-                    temp[i][j2] = (temp[i][j2] + v) % mod;
-                    temp[i][j] = (temp[i][j] + v) % mod;
+                    tempDP[i2][j] = (tempDP[i2][j] + v) % mod;
+                    tempDP[i][j2] = (tempDP[i][j2] + v) % mod;
                 }
             }
-            dp = temp;
+            // copy back
+            for (int i = M; i >= 0; --i) {
+                for (int j = M; j >= 0; --j) {
+                    dp[i][j] = tempDP[i][j];
+                }
+            }
         }
         int res = 0;
         for (int i = 1; i <= M; ++i) {
